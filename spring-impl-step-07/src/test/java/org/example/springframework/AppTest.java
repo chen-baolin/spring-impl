88ce8416f58ca02cfbab7bda0735228912ed4efd
chenbl <chenbaolin2020@163.com>
2024-08-09 12:04:07 +0800

    初始化和销毁方法

---
+++ spring-impl-step-07/src/test/java/org/example/springframework/AppTest.java
@@ -0,0 +1,55 @@
+package org.example.springframework;
+
+import org.example.springframework.beans.PropertyValue;
+import org.example.springframework.beans.PropertyValues;
+import org.example.springframework.beans.factory.config.BeanDefinition;
+import org.example.springframework.beans.factory.config.BeanReference;
+import org.example.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.example.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.example.springframework.context.support.ClassPathXmlApplicationContext;
+import org.example.springframework.test.bean.UserDao;
+import org.example.springframework.test.bean.UserService;
+import org.example.springframework.test.common.MyBeanFactoryPostProcessor;
+import org.example.springframework.test.common.MyBeanPostProcessor;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testBeanFactoryPostProcessorAndBeanPostProcessor(){
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2. 读取配置文件&注册Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
+
+        // 3. BeanDefinition 加载完成 & Bean实例化之前，修改 BeanDefinition 的属性值
+        MyBeanFactoryPostProcessor beanFactoryPostProcessor = new MyBeanFactoryPostProcessor();
+        beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
+
+        // 4. Bean实例化之后，修改 Bean 属性信息
+        MyBeanPostProcessor beanPostProcessor = new MyBeanPostProcessor();
+        beanFactory.addBeanPostProcessor(beanPostProcessor);
+
+        // 5. 获取Bean对象调用方法
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+    }
+
+    @Test
+    public void testXml() {
+        // 1.创建上下文对象
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        // 2.获取Bean对象调用方法
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+    }
+}
