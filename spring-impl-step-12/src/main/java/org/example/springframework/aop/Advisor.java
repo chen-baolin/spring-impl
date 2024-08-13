f2be9c294ed76c38d9c6b9df01d9ef7fc23b142b
chenbl <chenbaolin2020@163.com>
2024-08-13 17:53:32 +0800

    aop扩展到bean的生命周期

---
+++ spring-impl-step-12/src/main/java/org/example/springframework/aop/Advisor.java
@@ -0,0 +1,8 @@
+package org.example.springframework.aop;
+
+import org.aopalliance.aop.Advice;
+
+public interface Advisor {
+
+    Advice getAdvice();
+}
---
+++ spring-impl-step-13/src/main/java/org/example/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,8 @@
+package org.example.springframework.beans.factory.config;
+
+import org.example.springframework.beans.BeansException;
+
+public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
+
+    Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException;
+}
